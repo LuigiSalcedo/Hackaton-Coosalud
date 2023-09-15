2784f3634fe0e690953f652c8f347fd5753a4485
Luigi Salcedo <csalcedoe@unicartagena.edu.co>
2023-09-15 12:30:18 -0500

    API Rest - Create users

---
+++ portal/src/main/java/com/coosalud/hackaton/portal/domain/controllers/CoouserController.java
@@ -0,0 +1,44 @@
+package com.coosalud.hackaton.portal.domain.controllers;
+
+import com.coosalud.hackaton.portal.domain.models.users.Coouser;
+import com.coosalud.hackaton.portal.domain.models.users.CoouserCreatorDto;
+import com.coosalud.hackaton.portal.domain.models.users.CoouserResponseDto;
+import com.coosalud.hackaton.portal.domain.repositories.CoouserRepository;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import java.net.URI;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+/**
+ *
+ * @author Luigi Salcedo
+ */
+@RestController
+@RequestMapping("/users")
+public class CoouserController {
+    
+    @Autowired
+    private CoouserRepository repository;
+    
+    @PostMapping("/create")
+    @Transactional
+    public ResponseEntity saveCoouser(@RequestBody @Valid CoouserCreatorDto data, 
+            UriComponentsBuilder uriBuilder) {
+        Coouser coouser = repository.save(new Coouser(data));
+        
+        CoouserResponseDto jsonResponse = new CoouserResponseDto(coouser);
+        
+        URI url = uriBuilder.path("/user/info/{id}")
+                            .buildAndExpand(coouser.getId())
+                            .toUri();
+        
+        return ResponseEntity.created(url).body(jsonResponse);
+    }
+}
---
+++ portal/src/main/java/com/coosalud/hackaton/portal/domain/models/users/Coouser.java
@@ -0,0 +1,80 @@
+package com.coosalud.hackaton.portal.domain.models.users;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.Collection;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+/**
+ *
+ * @author Luigi Salcedo
+ */
+@Getter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="coouser")
+@Entity
+public class Coouser implements UserDetails {
+    @Id
+    @Column(name="id")
+    private Integer id;
+    @Column(name="name")
+    private String name;
+    @Column(name="email")
+    private String email;
+    @Column(name="password")
+    private String password;
+
+    /**
+     * This constructor is used to create an using the JSON data recived from
+     * the frontend.
+     * 
+     * @param data record with the data in JSON format.
+     */
+    public Coouser(CoouserCreatorDto data) {
+        this.id = data.id();
+        this.name = data.name();
+        this.email = data.email();
+        this.password = data.password();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getUsername() {
+        return String.valueOf(id);
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
